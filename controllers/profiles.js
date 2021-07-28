bb721d89295e8792af066bb29c58be47059267f3
HenryJrA <henryjrallam7@gmail.com>
2021-07-27 21:51:56 -0400

    edited

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -1,9 +1,17 @@
 import { Profile } from "../models/profile.js"
 
 export{
-    newProfile as new 
+    newProfile as new,
+    create 
 }
   
 function newProfile(req, res) {
     res.render('profiles/new')
 }
+function create(req, res) {
+    Profile.create(req.body)
+    .then(() =>{
+    res.redirect('/profiles')
+    })
+    
+  }
