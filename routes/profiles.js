ebcbd19587364392773c89fa057bff12d495345f
HenryJrA <henryjrallam7@gmail.com>
2021-07-28 22:29:51 -0400

    crud

--- routes/profiles.js
+++ routes/profiles.js
@@ -3,7 +3,7 @@ const router = Router()
 import * as profilesCtrl from '../controllers/profiles.js'
 
 router.get('/', profilesCtrl.index)
-// router.post('/', profilesCtrl.create)
+router.post('/', profilesCtrl.create)
 // router.delete('/:id', profilesCtrl.delete)
 // router.get('/new', profilesCtrl.new)
 
