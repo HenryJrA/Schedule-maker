4d295a6c5eca6bd231f726705e8cff77a9a02486
HenryJrA <henryjrallam7@gmail.com>
2021-07-28 22:30:26 -0400

    crud

--- routes/profiles.js
+++ routes/profiles.js
@@ -4,7 +4,7 @@ import * as profilesCtrl from '../controllers/profiles.js'
 
 router.get('/', profilesCtrl.index)
 router.post('/', profilesCtrl.create)
-// router.delete('/:id', profilesCtrl.delete)
+router.delete('/:id', profilesCtrl.delete)
 // router.get('/new', profilesCtrl.new)
 
 export {
